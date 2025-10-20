80bd1fcbc3d4c95a9881a26addceb5c00c085d34
Neev <neev.nedungadi@gmail.com>
2025-10-20 17:37:19 -0400

    adding outtake + pneumatics

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace pros;
 // ==================== CONFIG (EDIT THESE) ====================
 constexpr int8_t L_FRONT_PORT = -20;
 constexpr int8_t L_BACK_PORT  = -19;
-constexpr int8_t R_FRONT_PORT = 10;
+constexpr int8_t R_FRONT_PORT = 7;
 constexpr int8_t R_BACK_PORT  = 9;
 
 // Odometry ports
@@ -15,21 +15,24 @@ constexpr int8_t VERTICAL_ROTATION_SENSOR_PORT = 8;
 // constexpr int8_t HORIZONTAL_ROTATION_SENSOR_PORT = 16;
 
 // Intake ports
-//Clockwise Direction
-constexpr int8_t OUTER_TOWER_MIDDLE_INTAKE_PORT = -12;
-constexpr int8_t INNER_TOWER_MIDDLE_TOP_INTAKE_PORT = -11;
-constexpr int8_t INNER_TOWER_LOWER_INTAKE_PORT = 13;
+// Clockwise Direction
+constexpr int8_t OUTER_TOWER_MIDDLE_INTAKE_PORT      = -12;
+constexpr int8_t INNER_TOWER_MIDDLE_TOP_INTAKE_PORT  = -11;
+constexpr int8_t INNER_TOWER_LOWER_INTAKE_PORT       = 13;
 
 // Outtake ports - todo
-constexpr int8_t OUTTAKE_PORT = 14;
+constexpr int8_t TOP_OUTTAKE_PORT = 14;
+
+// Pneumatics (3-wire/ADI) port
+constexpr char PISTON_PORT = 'H';  // Change to your 3-wire port (A..H)
 
 // Drive style
 constexpr bool kArcadeDrive = true;
 
 // Shaping
 constexpr int    kDeadband = 5;
-constexpr double kExpo     = 1.6;
-constexpr int    kMaxCmd   = 127;
+constexpr double kExpo      = 1.6;
+constexpr int    kMaxCmd    = 127;
 
 // Loop rate
 constexpr int kDriveLoopMs  = 10;
@@ -47,11 +50,14 @@ constexpr auto BTN_INLOWSTORAGE = E_CONTROLLER_DIGITAL_R2;
 constexpr auto BTN_OUTMIDGOAL   = E_CONTROLLER_DIGITAL_L1;
 constexpr auto BTN_OUTLOWGOAL   = E_CONTROLLER_DIGITAL_L2;
 constexpr auto BTN_INTAKE_OFF   = E_CONTROLLER_DIGITAL_X;
-constexpr auto BTN_OUTTAKE      = E_CONTROLLER_DIGITAL_Y;
+constexpr auto BTN_TOPOUTTAKE   = E_CONTROLLER_DIGITAL_Y;
 
+// Pneumatic toggle button
+constexpr auto BTN_PISTON_TOGGLE = E_CONTROLLER_DIGITAL_RIGHT;
 
 // ==================== DEVICES ====================
 Controller master(E_CONTROLLER_MASTER);
+
 // Drive train motor objects
 MotorGroup leftDrive({L_FRONT_PORT, L_BACK_PORT}, v5::MotorGears::green, v5::MotorUnits::rotations);
 MotorGroup rightDrive({R_FRONT_PORT, R_BACK_PORT}, v5::MotorGears::green, v5::MotorUnits::rotations);
@@ -60,6 +66,14 @@ MotorGroup rightDrive({R_FRONT_PORT, R_BACK_PORT}, v5::MotorGears::green, v5::Mo
 Motor outerTowerMiddleMotor(OUTER_TOWER_MIDDLE_INTAKE_PORT, pros::v5::MotorGears::green, pros::v5::MotorUnits::rotations);
 Motor innerTowerMiddleTopMotor(INNER_TOWER_MIDDLE_TOP_INTAKE_PORT, pros::v5::MotorGears::green, pros::v5::MotorUnits::rotations);
 Motor innerTowerLowerMotor(INNER_TOWER_LOWER_INTAKE_PORT, pros::v5::MotorGears::green, pros::v5::MotorUnits::rotations);
+
+
+// Outtake Motor
+Motor topOutTakeMotor(TOP_OUTTAKE_PORT, pros::v5::MotorGears::blue, pros::v5::MotorUnits::rotations);
+
+// ==================== PNEUMATICS ====================
+pros::adi::Pneumatics piston(PISTON_PORT, false); // false = retracted at startup
+static bool piston_extended = false;
 
 // ==================== HELPERS ====================
 static inline int clamp127(int v) { return std::max(-127, std::min(127, v)); }
@@ -88,26 +102,23 @@ static inline void driveArcade(int throttle, int turn) {
 // ==================== INTAKE SUBSYSTEM ====================
 class Intake {
 public:
-  enum class Mode { Off, InTopStorage, InLowStorage, OutMiddleGoal, OutLowGoal};
+  enum class Mode { Off, InTopStorage, InLowStorage, OutMiddleGoal, OutLowGoal, OutTopGoal};
 
-  Intake(Motor& L1, Motor& L2, Motor& R1) : m_outerTowerMiddleMotor(L1),
-                                            m_innerTowerMiddleTopMotor(L2), 
-                                            m_innerTowerLowerMotor(R1) {}
+  Intake(Motor& L1, Motor& L2, Motor& R1, Motor& T1)
+  : m_outerTowerMiddleMotor(L1), m_innerTowerMiddleTopMotor(L2), m_innerTowerLowerMotor(R1),m_topGoalMotor(T1) {}
 
-  void set_mode(Mode m) {
-    mode_ = m;
-  }
-
+  void set_mode(Mode m) { mode_ = m; }
   Mode mode() const { return mode_; }
 
   // Call from a background task
   void update() {
-    // TODO - need to ramp up speed rather than just go with full voltage
+    // TODO - ramp speeds instead of instant full voltage
     switch (mode_) {
-      case Mode::Off: 
+      case Mode::Off:
         m_outerTowerMiddleMotor.move(0);
         m_innerTowerMiddleTopMotor.move(0);
         m_innerTowerLowerMotor.move(0);
+        m_topGoalMotor.move(0);
         break;
       case Mode::InTopStorage:
         m_outerTowerMiddleMotor.move(127);
@@ -117,10 +128,13 @@ public:
       case Mode::InLowStorage:
         m_outerTowerMiddleMotor.move(127);
         m_innerTowerLowerMotor.move(0);
-        m_innerTowerMiddleTopMotor.move(127);  
+        m_innerTowerMiddleTopMotor.move(127);
         break;
       case Mode::OutMiddleGoal:
       case Mode::OutLowGoal:
+      case Mode::OutTopGoal:
+        m_topGoalMotor.move(127);
+        break;
       default:
         m_outerTowerMiddleMotor.move(0);
         m_innerTowerMiddleTopMotor.move(0);
@@ -133,15 +147,17 @@ private:
   Motor& m_outerTowerMiddleMotor;
   Motor& m_innerTowerMiddleTopMotor;
   Motor& m_innerTowerLowerMotor;
-  Mode mode_  = Mode::Off;
+  Motor& m_topGoalMotor;
+  Mode mode_ = Mode::Off;
 };
 
-Intake intake(outerTowerMiddleMotor, innerTowerMiddleTopMotor, innerTowerLowerMotor);
+Intake intake(outerTowerMiddleMotor, innerTowerMiddleTopMotor, innerTowerLowerMotor, topOutTakeMotor);
 
 void intakeTaskFn(void*) {
   outerTowerMiddleMotor.set_brake_mode(E_MOTOR_BRAKE_COAST);
   innerTowerMiddleTopMotor.set_brake_mode(E_MOTOR_BRAKE_COAST);
   innerTowerLowerMotor.set_brake_mode(E_MOTOR_BRAKE_COAST);
+  topOutTakeMotor.set_brake_mode(E_MOTOR_BRAKE_COAST);
   while (true) { intake.update(); delay(kIntakeLoopMs); }
 }
 Task intakeTask(intakeTaskFn, nullptr, "intake_task");
@@ -149,7 +165,7 @@ Task intakeTask(intakeTaskFn, nullptr, "intake_task");
 // ==================== PROS LIFECYCLE ====================
 void initialize() {
   lcd::initialize();
-  lcd::set_text(1, "Drive+Intake Ready");
+  lcd::set_text(1, "Drive+Intake+Outtake+Piston Ready");
 
   leftDrive.set_brake_mode(E_MOTOR_BRAKE_COAST);
   rightDrive.set_brake_mode(E_MOTOR_BRAKE_COAST);
@@ -160,6 +176,10 @@ void initialize() {
   outerTowerMiddleMotor.tare_position();
   innerTowerMiddleTopMotor.tare_position();
   innerTowerLowerMotor.tare_position();
+
+  // Pneumatic default: retracted
+  piston.set_value(false);
+  piston_extended = false;
 }
 
 void disabled() {}
@@ -184,12 +204,6 @@ void autonomous() {
 
 // --------- Driver Control ---------
 void opcontrol() {
-  /* 
-  intakeL1Toggle = false;
-  intakeL2Toggle = false;
-  intakeR1Toggle = false;
-  */
-
   while (true) {
     // Brake mode quick toggle
     if (master.get_digital_new_press(BTN_BRAKE_HOLD)) {
@@ -202,53 +216,19 @@ void opcontrol() {
       rightDrive.set_brake_mode_all(MotorBrake::coast);
       lcd::set_text(2, "Drive Brake: COAST");
     }
-    
-/*
-    // Intake Motors Test
-    if (master.get_digital_new_press(BTN_INTAKE)) {
-      intakeL1Toggle = !intakeL1Toggle;
-      if (intakeL1Toggle) {
-        innerTowerLowerMotor.move(127);
-      } else {
-        innerTowerLowerMotor.move(0);
-      }
+
+    // ----- Pneumatics: toggle extend/retract -----
+    if (master.get_digital_new_press(BTN_PISTON_TOGGLE)) {
+      piston_extended = !piston_extended;
+      piston.set_value(piston_extended);  // true = extend, false = retract
+      lcd::set_text(3, piston_extended ? "Piston: EXTENDED" : "Piston: RETRACTED");
     }
-
-    if (master.get_digital_new_press(BTN_OUTTAKE)) {
-      intakeL2Toggle = !intakeL2Toggle;
-      if (intakeL2Toggle) {
-        innerTowerMiddleTopMotor.move(127);
-      } else {
-        innerTowerMiddleTopMotor.move(0);
-      }
-    }
-    
-    if (master.get_digital_new_press(BTN_HOLD)) {
-      intakeR1Toggle = !intakeR1Toggle;
-      if (intakeR1Toggle) {
-        outerTowerMiddleMotor.move(-127);
-      } else {
-        outerTowerMiddleMotor.move(0);
-      }
-    }
-    // Optional quick telemetry
-    lcd::set_text(2, "innerTowerLowerMotor:" + std::string(intakeL1Toggle ? "ON " : "OFF") +
-                     "innerTowerMiddleTopMotor:" + std::string(intakeL2Toggle ? "ON " : "OFF") +
-                     "outerTowerMiddleMotor:" + std::string(intakeR1Toggle ? "ON" : "OFF"));
-*/
-
-
-
-constexpr auto BTN_INTAKE_OFF   = E_CONTROLLER_DIGITAL_X;
-constexpr auto BTN_OUTTAKE      = E_CONTROLLER_DIGITAL_Y;
-
 
     // Intake Mode selection
     if (master.get_digital_new_press(BTN_INTOPSTORAGE)) {
       intake.set_mode(Intake::Mode::InTopStorage);
       lcd::set_text(2, "Intake Mode: InTopStorage");
     }
-
     if (master.get_digital_new_press(BTN_INLOWSTORAGE)) {
       intake.set_mode(Intake::Mode::InLowStorage);
       lcd::set_text(2, "Intake Mode: InLowStorage");
@@ -257,17 +237,19 @@ constexpr auto BTN_OUTTAKE      = E_CONTROLLER_DIGITAL_Y;
       intake.set_mode(Intake::Mode::OutMiddleGoal);
       lcd::set_text(2, "Intake Mode: OutMiddleGoal");
     }
-
     if (master.get_digital_new_press(BTN_OUTLOWGOAL)) {
       intake.set_mode(Intake::Mode::OutLowGoal);
       lcd::set_text(2, "Intake Mode: OutLowGoal");
     }
-
     if (master.get_digital_new_press(BTN_INTAKE_OFF)) {
       intake.set_mode(Intake::Mode::Off);
       lcd::set_text(2, "Intake Mode: Off");
     }
     // No code defined yet for outtake
+    if (master.get_digital_new_press(BTN_TOPOUTTAKE)) {
+      intake.set_mode(Intake::Mode::OutTopGoal);
+      lcd::set_text(2, "Intake Mode: OutTopGoal");
+    }
 
     // Drive input
     int LY = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
